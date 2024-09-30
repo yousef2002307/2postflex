4f610f5c3e1f7afa97e22e862ffe38e5d9f4a07c
yousef2002307 <85695400+yousef2002307@users.noreply.github.com>
2024-09-30 15:26:12 +0300

    First Commit

---
+++ inc/themes/backend/Stackmin/Config.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'id' => 'stackmin',
+    'folder' => 'Backend',
+    'name' => 'Stackmin',
+    'author' => 'Stackcode',
+    'author_uri' => 'https://stackposts.com',
+    'desc' => '',
+    'icon' => '',
+    'color' => '#ccc',
+    'require' => true
+];
